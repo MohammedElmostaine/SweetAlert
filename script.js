0a63c8aea0069333c23e9b7321949208549de7d0
MOHAMED EL MOSTAINE <mohammedelmostaine@gmail.com>
2024-12-07 16:12:38 +0100

    success function and btn

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
-import  { sweetinfo} from "./sweet.js";
+import  { sweetinfo , sweetSuccess } from "./sweet.js";
 
-let btn1 = document.getElementById('btn1')
+let btn1 = document.getElementById('btn1');
+let btn2 = document.getElementById('btn2');
+
+
+
+let successMessage = "This is a Success Alert!";
+let Success="Success";
+
+
 
 let insiMessage = "This is Info Alert";
 btn1.addEventListener("click", () => {
     sweetinfo(insiMessage);
 });
+
+
+btn2.addEventListener("click", () => {
+    sweetSuccess(successMessage ,Success);
+});
+
--- sweet.js
+++ sweet.js
@@ -31,21 +31,44 @@ function sweetinfo(message ,info) {
 }
 
 
+function sweetSuccess(message ,Success) {
+    const bodyofSuccessalert = document.createElement("div");
+    bodyofSuccessalert.classList.add("sweetSuccessSec" , "sweetSec");
+    bodyofSuccessalert.innerHTML = `<div class ="sweet" id="sweetSuccess">
+                            <h1 class ="title" id = "Successtitle">${Success}&nbsp <i class="fa-solid fa-check"></i></h1>
+                            <p class ="Parag" id = "SuccessParag">${message}</p>
+                            <i  id = "iconeCloseSuccess" class="iconeClose fa-solid fa-circle-xmark" style="color: #000000; "></i>
+                            <hr>
+                            </div>  `;
+
+    const body = document.querySelector("body");
+    body.appendChild(bodyofSuccessalert);
+    const iconeCloseSuccess = document.querySelector("#iconeCloseSuccess");
+    iconeCloseSuccess.addEventListener("click", ()=> {
+        const sweetSucSec = document.querySelector(".sweetSuccessSec"); 
+    if (sweetSucSec) {
+        sweetSucSec.remove();
+    }
+    })
+    setTimeout(() => {
+        if (sweetSucSec) {
+            sweetSucSec.remove();
+        }
+    }, 3000);
+   
+}
 
 
 
 
 
 
-export { sweetinfo , sweetSuccess};
 
 
-// <div class ="sweetSec" >
-//         <div class ="sweet" id="sweetinfo">
-//             <h1 class ="title" id = "infotitle">Info Alert</h1>
-//             <p class ="Parag" id = "infoParag">${message}</p>
-//             <i  id = "iconeClose" class="iconeClose fa-solid fa-circle-xmark" style="color: #000000; "></i>
-//             <hr class="infohr">
-//         </div>
-//         </div>
+
+
+export { sweetinfo , sweetSuccess };
+
+
+
 
