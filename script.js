a323897bd6079c88ffeff6491a0ce554796aa203
MOHAMED EL MOSTAINE <mohammedelmostaine@gmail.com>
2024-12-07 16:18:39 +0100

    warning function and btn

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-import  { sweetinfo , sweetSuccess } from "./sweet.js";
+import  { sweetinfo , sweetSuccess , sweetWarning} from "./sweet.js";
 
 let btn1 = document.getElementById('btn1');
 let btn2 = document.getElementById('btn2');
-
+let btn3 = document.getElementById('btn3');
 
 
 let successMessage = "This is a Success Alert!";
 let Success="Success";
 
-
+let warningMessage = "This is a Warning Alert!";
+let warningTitle = "Warning";
 
 let insiMessage = "This is Info Alert";
 btn1.addEventListener("click", () => {
@@ -20,3 +21,6 @@ btn2.addEventListener("click", () => {
     sweetSuccess(successMessage ,Success);
 });
 
+btn3.addEventListener("click", () => {
+    sweetWarning(warningMessage, warningTitle);
+});
